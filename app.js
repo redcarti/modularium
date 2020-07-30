a2ee088358bb4d9f15e4bfe200b462051656ca35
redcarti <ppamperr@gmail.com>
2020-07-30 16:20:49 +0500

    removed fox

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
-const { FoxDispatcher } = require('./src/lib/Fox')
 const { RopeModule } = require('./src/lib/Rope')
 
 module.exports = {
     run: (config) => {
         require('./src/index').run(config)
     },
-    FoxDispatcher,
     RopeModule
 }
