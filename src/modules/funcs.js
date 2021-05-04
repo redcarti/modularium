21d5ac276627a1bc4021f1f6d3f9615a823d2318
redcarti <ppamperr@gmail.com>
2021-05-04 18:02:10 +0500

    fix: typo discriminator

--- src/modules/funcs.js
+++ src/modules/funcs.js
@@ -77,5 +77,5 @@ module.exports = (pl, { user }) => {
     return emoji
   }
 
-  pl.userWithDescriminator = user => `${user.username}#${user.descriminator}`
+  pl.userWithDiscriminator = user => `${user.username}#${user.discriminator}`
 }
