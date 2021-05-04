55e308f7e058722024c77cf8e5179e27842e4745
redcarti <ppamperr@gmail.com>
2021-05-04 17:37:41 +0500

    fix(updater): fix uncorrect import

--- src/modules/updater.js
+++ src/modules/updater.js
@@ -1,5 +1,5 @@
 const pkg = require('../../package.json')
-const checkUpdate = require('@modularium/check-update')
+const { checkUpdate } = require('@modularium/check-update')
 
 /**
  * Updater module
